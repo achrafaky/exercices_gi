1365b0a71355e4a0d4dbe0de049f9ac441098c9d
achrafaky <akiyafachraf08@gmail.com>
2025-09-14 20:27:45 +0100

    day 2 xp gold

---
+++ week_3/day_2/exercicexpgold.js
@@ -0,0 +1,15 @@
+// 🌟 Exercise 1 : Divisible by three
+let numbers = [123, 8409, 100053, 333333333, 7];
+for (let i = 0; i < numbers.length; i++) { console.log(numbers[i] % 3 === 0); }
+
+// 🌟 Exercise 2 : Attendance
+let guestList = { randy: "Germany", karla: "France", wendy: "Japan", norman: "England", sam: "Argentina" };
+let name = prompt("What is your name?");
+if (name in guestList) { console.log(`Hi! I'm ${name}, and I'm from ${guestList[name]}.`); } else { console.log("Hi! I'm a guest."); }
+
+// 🌟 Exercise 3 : Playing with numbers
+let age = [20, 5, 12, 43, 98, 55];
+let sum = 0; for (let i = 0; i < age.length; i++) { sum += age[i]; }
+console.log("Sum:", sum);
+let highest = age[0]; for (let i = 1; i < age.length; i++) { if (age[i] > highest) { highest = age[i]; } }
+console.log("Highest age:", highest);
