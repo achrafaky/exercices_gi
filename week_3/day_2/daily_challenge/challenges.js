9d515cecb169bb21cd99fdbafb9081ac1b268f9d
achrafaky <akiyafachraf08@gmail.com>
2025-09-14 20:38:55 +0100

    challenge daily day 2

--- week_3/day_2/daily_challenge/challenges.js
+++ week_3/day_2/daily_challenge/challenges.js
@@ -1,5 +1,17 @@
-// ====== Daily Challenge 1
+// 🌟 Exercise : Replace "not ... bad" with "good"
+let sentence = "The movie is not that bad, I like it";
 
-// ====== Daily Challenge 2
+// Trouver la position de "not" et "bad"
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
 
-// ====== Daily Challenge 3
+// Vérification
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+  // Découper et remplacer la partie "not ... bad"
+  let result = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+  console.log(result);
+} else {
+  // Si "bad" ne vient pas après "not", afficher original
+  console.log(sentence);
+}
+
